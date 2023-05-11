11cedb0609aaaf7484aa76fdd959e14cf3469663
Abhinay2211 <45136687+Abhinay2211@users.noreply.github.com>
2023-05-11 01:34:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,39 @@
 import cv2
 import streamlit as st
 import numpy as np
-from PIL import Image
+# import tempfile
+
+# Use this line to capture video from the webcam
+cap = cv2.VideoCapture(1)
 
 
-def brighten_image(image, amount):
-    img_bright = cv2.convertScaleAbs(image, beta=amount)
-    return img_bright
+# Set the title for the Streamlit app
+st.title("Video Capture with OpenCV")
 
+frame_placeholder = st.empty()
 
-def blur_image(image, amount):
-    blur_img = cv2.GaussianBlur(image, (11, 11), amount)
-    return blur_img
+# Add a "Stop" button and store its state in a variable
+stop_button_pressed = st.button("Stop")
 
+while cap.isOpened() and not stop_button_pressed:
+    ret, frame = cap.read()
 
-def enhance_details(img):
-    hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
-    return hdr
+    if not ret:
+        st.write("The video capture has ended.")
+        break
 
+    # You can process the frame here if needed
+    # e.g., apply filters, transformations, or object detection
 
-def main_loop():
-    st.title("OpenCV Demo App")
-    st.subheader("This app allows you to play with Image filters!")
-    st.text("We use OpenCV and Streamlit for this demo")
+    # Convert the frame from BGR to RGB format
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+    # Display the frame using Streamlit's st.image
+    frame_placeholder.image(frame, channels="RGB")
 
-    image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    if not image_file:
-        return None
+    # Break the loop if the 'q' key is pressed or the user clicks the "Stop" button
+    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed: 
+        break
 
-    original_image = Image.open(image_file)
-    original_image = np.array(original_image)
-
-    processed_image = blur_image(original_image, blur_rate)
-    processed_image = brighten_image(processed_image, brightness_amount)
-
-    if apply_enhancement_filter:
-        processed_image = enhance_details(processed_image)
-
-    st.text("Original Image vs Processed Image")
-    st.image([original_image, processed_image])
-
-
-if __name__ == '__main__':
-    main_loop()
+cap.release()
+cv2.destroyAllWindows()
